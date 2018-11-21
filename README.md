ee05e6957990b62c2d02a0b67b020c0e42442fec
r kip <gjr.kip@gmail.com>
2018-11-21 13:57:19 +0100

    first setup

---
+++ README.md
@@ -0,0 +1,6 @@
+#Installation
+pip3 install gym
+pip3 install box2d-py
+
+#Running
+python3 environment.py
---
+++ enivronment.py
@@ -0,0 +1,7 @@
+import gym
+env = gym.make("BipedalWalker-v2")
+observation = env.reset()
+for _ in range(1000):
+  env.render()
+  action = env.action_space.sample() # your agent here (this takes random actions)
+  observation, reward, done, info = env.step(action)
