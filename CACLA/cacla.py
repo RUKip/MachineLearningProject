430da9bc357b80929c20d6e23e83ce8b0d7fac19
Diego Cabo <dcgdiego@gmail.com>
2018-12-14 13:18:26 +0100

    Corrected actions target.

--- CACLA/cacla.py
+++ CACLA/cacla.py
@@ -7,6 +7,8 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 import sys
+import progressbar
+import logging
 
 
 EPISODES = 10000
@@ -24,8 +26,8 @@ class CACLACritic:
 
     def _build_model(self):  # Neural Net for CACLA learning Model - critic
         model = Sequential()
-        model.add(Dense(400, input_dim=self.state_size, activation='relu'))
-        model.add(Dense(400, activation='relu'))
+        model.add(Dense(50, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(50, activation='relu'))
         model.add(Dense(1, activation='linear'))  # Returns the Value for best policy
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
         return model
@@ -33,6 +35,10 @@ class CACLACritic:
     def learn(self, state, reward, next_state):
         target = reward + self.gamma*self.model.predict(next_state)
         self.model.fit(state, target, epochs=1, verbose=0)
+
+    def getTDE(self, state, reward, next_state):
+        target = reward + self.gamma*self.model.predict(next_state)
+        return target - self.model.predict(state)
 
     def load(self, name):
         self.model.load_weights(name)
@@ -47,15 +53,14 @@ class CACLAActor:
         self.action_size = action_size
         self.sigma = 1.0
         self.sigma_min = 0.01
-        # self.sigma_decay = 0.9995
         self.sigma_decay = 0.999
         self.learning_rate = 0.001
         self.model = self._build_model()
 
     def _build_model(self):  # Neural Net for CACLA learning Model - actor
         model = Sequential()
-        model.add(Dense(400, input_dim=self.state_size, activation='relu'))
-        model.add(Dense(400, activation='relu'))
+        model.add(Dense(50, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(50, activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))
         model.compile(loss='mse',
                       optimizer=Adam(lr=self.learning_rate))
@@ -68,9 +73,9 @@ class CACLAActor:
             actions.append(np.random.normal(val, self.sigma))
         return np.array(actions)
 
-    def learn(self, state, action, value_state, value_nextState):
-        if value_nextState > value_state:
-            target = action - self.model.predict(state)
+    def learn(self, state, action, tempDiffErr):
+        if tempDiffErr > 0:
+            target = action
             self.model.fit(state, target, epochs=1, verbose=0)
             # if self.sigma > self.sigma_min:
             #     self.sigma *= self.sigma_decay
@@ -83,6 +88,8 @@ class CACLAActor:
 
 
 if __name__ == "__main__":
+
+    logging.basicConfig(filename='output.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
     env = gym.make('BipedalWalker-v2')
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.shape[0]
@@ -100,12 +107,12 @@ if __name__ == "__main__":
         X_vel_offset = float(sys.argv[2])
         actor_filename = sys.argv[3]
         critic_filename = sys.argv[4]
-    print("actor_filename: {}, critic_filename: {}, sigmadecay: {}, xvel: {}".format(actor_filename, critic_filename, actor.sigma_decay, X_vel_offset))
+    logging.info("Starting exercise: actor_filename: {}, critic_filename: {}, sigmadecay: {}, xvel: {}".format(actor_filename, critic_filename, actor.sigma_decay, X_vel_offset))
 
     # actor.load(actor_filename)
     # critic.load(critic_filename)
 
-    for e in range(EPISODES):
+    for e in progressbar.progressbar(range(EPISODES)):
         state = env.reset()
         state = np.reshape(state, [1, state_size])
         # Initialize the X position after reset. We will use this to calculate reward
@@ -120,31 +127,31 @@ if __name__ == "__main__":
             next_state, reward, done, _ = env.step(action)
 
             velX_array.append(state[0][2])
-
             posX = integrate.trapz(velX_array, dx=dt)
-            posRewardFactor = 10.0
-
+            posRewardFactor = 20.0
             reward += posRewardFactor*posX
 
             # TODO: WHAT IF WE INTEGRATE THE VEL TO GET POS AND THEN REWARD BASED ON HOW FAR WE MOVED
-            # x_vel = state[0][2]
+            x_vel = state[0][2]
             # r_X_vel = X_vel_offset
-            # # r_X_vel = X_vel_offset + actor.sigma*100.0
             # if x_vel > 0:
             #     reward += r_X_vel*x_vel
-            # # print('vel x = {}, reward = {}'.format(x_vel, reward))
 
             next_state = np.reshape(next_state, [1, state_size])
-            actor.learn(state, action, critic.model.predict(state), critic.model.predict(next_state))
+            action = np.reshape(action, [1, action_size])
+            # logging.debug("actions: {}".format(action))
+            tempDiffErr = critic.getTDE(state, reward, next_state)
+            actor.learn(state, action, tempDiffErr)
             critic.learn(state, reward, next_state)
-
             state = next_state
-            if done:
-                print("episode: {}/{}, reward: {}, sigma: {:.2}" .format(e, EPISODES, reward, actor.sigma))
-                print(" posX: {}, rewardposX: {}" .format(posX, posRewardFactor*posX))
-                if actor.sigma > actor.sigma_min:
-                   actor.sigma *= actor.sigma_decay
-        if e > 2000:
-            actor.save(actor_filename)
-            critic.save(critic_filename)
 
+            if done:
+                logging.debug("episode: {}/{}, reward: {}, sigma: {:.2}, posX: {}, velX: {}"
+                              .format(e, EPISODES, reward, actor.sigma, posX, x_vel))
+                if reward > 200:
+                    logging.warning("Hallelujah! reward: {}".format(reward))
+                if actor.sigma > actor.sigma_min:
+                    actor.sigma *= actor.sigma_decay
+        # if e > 2000:
+        actor.save(actor_filename)
+        critic.save(critic_filename)
